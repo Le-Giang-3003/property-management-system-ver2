72aefc33901f041b8629b6131db05d9eb6f666e3
Lê Giang <legiang30032004@gmail.com>
2026-02-13 23:17:14 +0700

    feat: config load appsettings.Local.json

--- PropertyManagementSystemVer2.Web/Program.cs
+++ PropertyManagementSystemVer2.Web/Program.cs
@@ -7,6 +7,8 @@ using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Configuration
+    .AddJsonFile("appsettings.Local.json", optional: true, reloadOnChange: true);
 // Add services to the container.
 builder.Services.AddRazorPages();
 
